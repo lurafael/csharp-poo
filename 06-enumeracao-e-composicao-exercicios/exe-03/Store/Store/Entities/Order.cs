08c06522b24fee1443d9136f85eabbaa830079f4
L. Ricci <lucasrafael.ricci@gmail.com>
2021-01-31 13:54:54 -0300

    Exercicio 03 - Store com as classes Order, OrderItem, OrderStatus, Product e enum OrderStatus

---
+++ 06-enumeracao-e-composicao-exercicios/exe-03/Store/Store/Entities/Order.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Store.Entities.Enums;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Store.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0.00;
+
+            foreach (OrderItem item in Items)
+            {
+                total += item.SubTotal();
+            }
+
+            return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Order moment: {Moment}");
+            sb.AppendLine($"Order status: {Status}");
+            sb.AppendLine($"Client: {Client.Name} ({Client.BirthDate}) - {Client.Email}");
+            sb.AppendLine("Order items:");
+
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine($"{item.Product.Name}, {item.Price}, {item.Quantity}" +
+                    $", Subtotal: ${item.SubTotal().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+
+            sb.AppendLine($"Total price: {Total().ToString("F2", CultureInfo.InvariantCulture)}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ 06-enumeracao-e-composicao-exercicios/exe-03/Store/Store/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Store.Entities
+{
+    class Product
+    {
+        public String Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
