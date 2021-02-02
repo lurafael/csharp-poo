235b6f1b2136d5e37ed2598f5024f14562912eca
Lucas Ricci <lucasrafael.ricci@gmail.com>
2021-02-02 11:44:02 -0300

    Exercício aproveitado"SavingsAndBusinessAccount" sobre a aula de sobreposicao, virtual, override e base, com as classes Account, BusinessAccount e SavingsAccount... Com anotações e testes no Program.cs

---
+++ 07-heranca-e-polimorfismo/03-exe-sobreposicao-sobrescrita/SobreposicaoVirtualOverrideBase/SobreposicaoVirtualOverrideBase/Entities/SavingsAccount.cs
@@ -0,0 +1,29 @@
+﻿namespace SobreposicaoVirtualOverrideBase.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        //Quando digitado override + Espaço, aparece a lista dos métodos que podem ser sobrescritos, dentre eles o Withdraw
+        //como definido com a palavra virtual
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount); //Chama o método Withdraw da classe Withdraw e...
+            Balance -= 2.0; //Desconta mais 2 reais que era o pedido no exercício
+        }
+    }
+}
