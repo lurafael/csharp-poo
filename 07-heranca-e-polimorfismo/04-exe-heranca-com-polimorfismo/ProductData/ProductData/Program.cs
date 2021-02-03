0475f1dbc346b02dd7306fc75dee1061cb756e33
Lucas Ricci <lucasrafael.ricci@gmail.com>
2021-02-02 22:47:04 -0300

    Exercício ProductData sobre as aulas de polimorfismo e herança, com as classes Product, UsedProduct e ImportedProduct

---
+++ 07-heranca-e-polimorfismo/04-exe-heranca-com-polimorfismo/ProductData/ProductData/Program.cs
@@ -0,0 +1,56 @@
+﻿using ProductData.Entities;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ProductData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int quantityOfProducts = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= quantityOfProducts; i++)
+            {
+                Console.WriteLine($"Product #{i} data: ");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char productType = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Name: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (productType == 'c')
+                    products.Add(new Product(name, price));
+
+                else if (productType == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+
+                    products.Add(new UsedProduct(name, price, manufactureDate));
+                }
+                else
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS");
+            foreach(Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
