1e06de1bacb7e642ba5526f1c30fc70831cedcd9
Lucas Ricci <lucasrafael.ricci@gmail.com>
2021-02-03 08:53:11 -0300

    Exercício SavingsAndBusinessAccount sobre a aula de classes abstratas, com as classes Account, SavingsAccount e BusinessAccount, com anotações no Program

---
+++ 07-heranca-e-polimorfismo/05-exe-classes-abstratas/SavingsAndBusinessAccount/SavingsAndBusinessAccount/Entities/Account.cs
@@ -0,0 +1,31 @@
+﻿namespace SavingsAndBusinessAccount.Entities
+{
+    abstract class Account //Uma classe abstrata não pode ser instanciada.
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
---
+++ 07-heranca-e-polimorfismo/05-exe-classes-abstratas/SavingsAndBusinessAccount/SavingsAndBusinessAccount/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using SavingsAndBusinessAccount.Entities;
+
+namespace SavingsAndBusinessAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> accounts = new List<Account>();
+
+            accounts.Add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+            accounts.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+            accounts.Add(new SavingsAccount(1003, "Bob", 500.0, 0.01));
+            accounts.Add(new BusinessAccount(1004, "Anna", 500.0, 500.0));
+
+            //Como a superclasse é genérica(é SavingsAccount e BusinessAccount também), consigo percorrer facilmente
+            //a lista pegando o valor total de saldo no banco por exemplo:
+            double sum = 0.0;
+
+            foreach(Account account in accounts)
+            {
+                sum += account.Balance;
+            }
+
+            Console.WriteLine($"Total balance: {sum.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            foreach (Account account in accounts)
+            {
+                account.Withdraw(10.0);
+            }
+
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine($"Updated balance for account {account.Number} : {account.Balance}");
+            }
+        }
+    }
+}
